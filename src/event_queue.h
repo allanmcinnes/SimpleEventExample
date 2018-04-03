ec76a622906fdc3c124416bd7cdf3e7db3f9518d
Allan McInnes <allanmcinnes@users.noreply.github.com>
2018-04-03 21:11:37 +1200

    Add some notes about the implementation

--- src/event_queue.h
+++ src/event_queue.h
@@ -1,4 +1,20 @@
 // Simple FIFO queue of events
+/*
+A few things worth noting about this queue implementation:
+
+- The append operation returns false if there's no room in the queue. This makes the append non-blocking, 
+  but means that it's up to the caller to ensure an event actually gets into the queue.
+
+- Retrieving a value from the queue is split into two operations. The `getNextEvent` operation 
+  returns a pointer to an event, but doesn't change the head of the queue (so multiple calls to 
+  `getNextEvent` access the same value). The `consumeNextEvent` operation removes the head of 
+  the queue. This split in operations is partly a design choice based on the Command-Query 
+  Separation Principle, and partly a way to minimize copying and dynamic memory allocation.
+ 
+- The queue is not thread-safe. To make it suitable for use in concurrent systems (or from an
+   interrupt context) you will need to add some locks ot calls to disable interrupts to
+   the functions in the public interface.
+*/
 
 #ifndef _EVENT_QUEUE_H
 #define _EVENT_QUEUE_H
