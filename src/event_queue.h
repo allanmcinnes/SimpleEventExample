232a74e8ad0952babb5535d1cdc7b8b443ec7303
Allan McInnes <allanmcinnes@users.noreply.github.com>
2018-05-10 18:32:34 +1200

    Update event_queue.h

--- src/event_queue.h
+++ src/event_queue.h
@@ -4,15 +4,21 @@ A few things worth noting about this queue implementation:
 
 - The append operation returns false if there's no room in the queue. This makes the append non-blocking, 
   but means that it's up to the caller to ensure an event actually gets into the queue.
+  
+- The queue implementation requires a caller to allocate and populate an event locally before it
+  is copied into the queue during the append operation. An alternative would be to make the queue
+  entirely responsible for manmaging event memory (making it a kind of memory pool). But trying to
+  keep track of events that have been allocated and not yet enqueued would make for a more
+  complex queue implementation. I opted to keep it simple here.
 
 - Retrieving a value from the queue is split into two operations. The `getNextEvent` operation 
   returns a pointer to an event, but doesn't change the head of the queue (so multiple calls to 
   `getNextEvent` access the same value). The `consumeNextEvent` operation removes the head of 
   the queue. This split in operations is partly a design choice based on the Command-Query 
-  Separation Principle, and partly a way to minimize copying and dynamic memory allocation.
+  Separation Principle, and partly a way to minimize copying.
  
 - The queue is not thread-safe. To make it suitable for use in concurrent systems (or from an
-   interrupt context) you will need to add some locks ot calls to disable interrupts to
+   interrupt context) you will need to add some locks or calls to disable interrupts to
    the functions in the public interface.
 */
 
